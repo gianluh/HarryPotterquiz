a9a0a29f8ffdedc88d4ac42ef4c98065a639a851
gianlu <gianlucademaio0@gmail.com>
2025-03-03 17:48:07 +0100

    .

--- script.js
+++ script.js
@@ -17,19 +17,3 @@ startButton.forEach(button => {
 let main = document.getElementById('main-content')
 let header = document.getElementById('header')
 let opening = document.getElementById('opening')
-
-main.style.visibility = 'hidden'
-main.style.opacity = '0'
-header.style.visibility = 'hidden'
-header.style.opacity = '0'
-
-opening.addEventListener('click', () => {
-    opening.hidden = true
-    main.style.visibility = 'visible'
-    main.style.opacity = '1'
-    main.style.transition = 'opacity 0.3s ease'
-    header.style.visibility = 'visible'
-    header.style.opacity = '1'
-    header.style.transition = 'opacity 0.3s ease'
-    
-});
